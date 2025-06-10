085ab5b8b893ab4884afd01faecf9336c58c10fb
Kyla Clarke <kylaclarke0405@gmail.com>
2025-06-10 11:03:47 -0400

    Working on a dark mode that isn't going live yet (more to come), but the button is fully responsive and keeps decision in local storage (commented out until dark mode is complete)

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+let darkmode = localStorage.getItem('dark-mode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('dark-mode')
+    localStorage.setItem('dark-mode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('dark-mode')
+    localStorage.setItem('dark-mode', null)
+}
+
+if(darkmode==="active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('dark-mode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode() //colon : is a fancy way of doing an if/else statement   
+})
